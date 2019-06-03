964bf7b65ef068df62f6d3dc5531e7227d61faec
George Miron <george.miron@ness.com>
2019-06-03 12:12:56 +0300

    first commit

---
+++ WebAPI/WebAPI/DbContext/Context.cs
@@ -0,0 +1,14 @@
+﻿using DTOs.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.DataBaseContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<Item> TodoItems { get; set; }
+    }
+}
